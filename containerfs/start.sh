ada6acebcaf659a1cc593af49f926631e4215062
Kai Mallea <kai@spotify.com>
2020-03-18 11:37:58 -0400

    Fix broken script

--- containerfs/start.sh
+++ containerfs/start.sh
@@ -72,7 +72,7 @@ exec ./srcds_run \
         -game csgo \
         -autoupdate \
         -steam_dir $STEAMCMD_DIR \
-        -steamcmd_script $CSGO_DIR/autoupdate_script.txt
+        -steamcmd_script $CSGO_DIR/autoupdate_script.txt \
         -tickrate $TICKRATE \
         -port $PORT \
         -tv_port $TV_PORT \
